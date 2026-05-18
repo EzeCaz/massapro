3d66ae5806cc361098d8ce3dbc6627508bda4070
Z User <z@container>
2026-05-18 06:33:40 +0000

    Add Google Analytics tag (G-Z2TP8Y923Q) to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -46,6 +46,20 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-Z2TP8Y923Q"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-Z2TP8Y923Q');
+          `}
+        </Script>
+        {/* Meta Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive">
           {`
             !function(f,b,e,v,n,t,s)
